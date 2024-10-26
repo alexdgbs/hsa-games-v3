92fe417f312a8789bae5a96a1ea3e1b830aba89f
alexdgbs <alexdbc24@gmail.com>
2024-10-25 23:38:43 -0600

    update11pm

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ app.use(express.static(path.join(__dirname, 'dist')));
 app.use(express.json());
 app.use(cors({
   origin: 'https://www.hsa-games.com', 
-  credentials: true, 
+  credentials: true
 }));
 app.use(helmet());
 
